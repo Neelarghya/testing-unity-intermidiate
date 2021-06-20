0861b4796c79c959cf59607e0e3de3f2fa29a578
Neelarghya Mandal <neelarghyamandal@Neelarghyas-MBP.Dlink>
2021-06-20 16:24:07 +0530

    [Neel] Adding simple editmode unit test

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,19 @@
+﻿public class Counter
+{
+    public int Count { get; private set; }
+
+    public void Reset()
+    {
+        Count = 0;
+    }
+    
+    public void Increment()
+    {
+        Count++;
+    }
+
+    public void IncrementBy(int amount)
+    {
+        Count += amount;
+    }
+}
---
+++ Assets/Tests/CounterTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class CounterTests
+    {
+        private Counter _counter;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _counter = new Counter();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _counter.Reset();
+        }
+        
+        [Test]
+        public void ShouldIncrementCounter()
+        {
+            Assert.Zero(_counter.Count);
+            _counter.Increment();
+            Assert.AreEqual(1, _counter.Count);
+        }
+        
+        [TestCase(10)]
+        [TestCase(20)]
+        [TestCase(30)]
+        public void ShouldIncrementCounterByParticularValue(int value)
+        {
+            Assert.Zero(_counter.Count);
+            _counter.IncrementBy(value);
+            Assert.AreEqual(value, _counter.Count);
+        }
+
+        [Ignore("For reasons")]
+        [Test]
+        public void ShouldResetCounterTo0()
+        {
+            _counter.Increment();
+            _counter.Reset();
+            Assert.Zero(_counter.Count);
+        }
+    }
+}
